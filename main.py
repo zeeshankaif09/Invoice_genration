53cd3e5f1889b19c54c61c1fb091728092a3864b
Zeeshankaif <zeeshankaif09@gmaail.com>
2023-01-13 03:05:21 +0530

    add logo

--- main.py
+++ main.py
@@ -43,4 +43,22 @@ for filepath in filepaths:
         pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
 
+    total_sum = df["total_price"].sum
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=" ", border=1)
+    pdf.cell(w=70, h=8, txt=" ", border=1)
+    pdf.cell(w=30, h=8, txt=" ", border=1)
+    pdf.cell(w=30, h=8, txt=" ", border=1)
+    pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
+
+    # add total sum
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.cell(w=30, h=8, txt=f"The total price is {total_sum} ", ln=1)
+
+    # add compny name and logo
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=26, h=8, txt=f"PYTHON", ln=1)
+    pdf.image("pythonhow.png", w=10)
+
     pdf.output(f"PDFS/{filename}.pdf")
